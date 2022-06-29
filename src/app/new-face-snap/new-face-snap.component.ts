af2f16c4e42214dae5a4fe096d1d6198753373be
Bastian Robillard <robillardbastian@gmail.com>
2022-06-29 16:09:18 +0200

    formulaire snap 2.0

--- src/app/new-face-snap/new-face-snap.component.ts
+++ src/app/new-face-snap/new-face-snap.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup } from '@angular/forms';
+import {FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import {map, Observable } from 'rxjs';
+import { FaceSnap } from '../modele/face-snap.model';
+import { TestGetService } from '../service/test-get.service';
 
 @Component({
   selector: 'app-new-face-snap',
@@ -8,20 +12,36 @@ import {FormBuilder, FormGroup } from '@angular/forms';
 })
 export class NewFaceSnapComponent implements OnInit {
   snapForm !: FormGroup;
-  constructor(private formBuilder : FormBuilder) { }
+  faceSnapPreview$ !: Observable<FaceSnap>;
+  urlRegex!: RegExp;
+
+  constructor(private formBuilder : FormBuilder,private service : TestGetService, private route :Router) { }
 
   ngOnInit(): void {
     this.snapForm =  this.formBuilder.group(
       {
-        title:[null],
-        description:[null],
-        imageUrl:[null],
-        location:[null]
+        title:[null, [Validators.required]],
+        description:[null,[Validators.required]],
+        imageUrl:[null, [Validators.required, Validators.pattern(this.urlRegex)]],
+        location:[null,[Validators.required]]
+      },{
+        upDateOn:'blur'
       }
     )
+    this.faceSnapPreview$ = this.snapForm.valueChanges.pipe(
+      map(formValue => ({
+        ...formValue,
+        createdDate: new Date(),
+        id:0,
+        snaps:0
+      }))
+    );
+    this.urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/;
   }
     onSubmitForm(): void{
-    console.log(this.snapForm.value);
+    console.log("on submit : "  + this.snapForm.value.imageUrl);
+    this.service.addFaceSnap(this.snapForm.value);
+    this.route.navigateByUrl('/facesnaps');
     }
 
 
